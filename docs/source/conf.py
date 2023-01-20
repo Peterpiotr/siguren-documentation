17392dce3a919eeee23a989050be6a54dc2063a7
Peter <peter.pachov@hotmail.fr>
2023-01-20 10:06:40 +0100

    -

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@ release = version
 
 # -- General configuration
 
+
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
@@ -42,22 +43,22 @@ epub_show_urls = 'footnote'
 
 html_logo = 'siguren_logo_white.png'
 
-#html_theme_options = {
+html_theme_options = {
 #    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
 #    'analytics_anonymize_ip': False,
-#    'logo_only': True,
+    'logo_only': True,
 #    'display_version': True,
 #    'prev_next_buttons_location': 'bottom',
 #    'style_external_links': False,
 #    'vcs_pageview_mode': '',
-#    'style_nav_header_background': '#2439bb',
+    'style_nav_header_background': '#2439bb',
     # Toc options
 #    'collapse_navigation': True,
 #    'sticky_navigation': True,
 #    'navigation_depth': 4,
 #    'includehidden': True,
 #    'titles_only': False
-#}
+}
 
 # These folders are copied to the documentation's HTML output
 html_static_path = ['_static']
